d9c1f4f9410ec526b2bc32d66e63a30423b58803
LuckyLuckyLu <alu8@stuy.edu>
2018-01-10 14:38:29 -0500

    Partially completed BioTrivia.java

--- BioTrivia.java
+++ BioTrivia.java
@@ -7,40 +7,78 @@ public class BioTrivia extends Activity{
       
       
       
-      int Score=0;
-      System.out.println("Welcome to the Biology Labratory! Since you seem so eager to be here, let's start with a little quiz!");
-	System.out.println("1. Let's do it!");
-	System.out.println("2. Well, *INSERT LONG EXCUSE*");
+	int Score=0;
+	System.out.println("Welcome to the Biology Labratory! Since you seem so eager to be here, let's start with a little quiz!"
+			   + " Select the best answer below by typing in the corresponding number.");
+	System.out.println("1. Biology? What fun. Let's do it!");	System.out.println("2. Well, I was forced to take ");
 	int FirstChoice = Keyboard.readInt();
 	if (FirstChoice == 1) {
-	    //Introduces Bio Quiz//System.out.println("");
-	    //First Question// System.out.println("");
+	    System.out.println("Good choice, welcome to the lab.");
+	    //Introduces Bio Quiz//
+	    System.out.println("");
+	    //First Question//
+	    System.out.println("What is the “powerhouse” of the cell? Is it:");
+	    //Choices//
+	    System.out.println("(1)Deoxyribonucleic Acid n\ (2)Mitochondria n\ (3)Cristae n\ (4)Ribosome");
 	    int FirstAnswer = Keyboard.readInt();
-	      if(FirstAnswer== 1) {
-		    System.out.println("MESSAGE THEY GET.");
-        Score+=1;
-		/*
-		  //Second Question//  System.out.println("");
-		    
-		}
+	    if(FirstAnswer== 2) {
+		Score+=1;
 	    }
-      if(Score<3;){
+	    //Second Question//
+	    System.out.println("What hormone is associated with stress?");
+	    //Choices//
+	    System.out.println("(1)Workasol \n (2)Cortisol \n (3)Life \n (4)Oxytocin");
+	    int SecondAnswer = Keyboard.readInt();
+	    if(SecondAnswer == 2) {
+		Score+=1;
+	    }
+	    //Third Question//
+	    System.out.println("What is the “powerhouse” of the cell? Is it:");
+	    //Choices//
+	    System.out.println("(1)Deoxyribonucleic Acid n\ (2)Mitochondria n\ (3)Cristae n\ (4)Ribosome");
+	    int SecondAnswer = Keyboard.readInt();
+	    if(SecondAnswer == 2) {
+		Score+=1;
+	    }
+	    //Fourth Question//
+	    System.out.println("What is the “powerhouse” of the cell? Is it:");
+	    //Choices//
+	    System.out.println("(1)Deoxyribonucleic Acid n\ (2)Mitochondria n\ (3)Cristae n\ (4)Ribosome");
+	    int SecondAnswer = Keyboard.readInt();
+	    if(SecondAnswer == 2) {
+		Score+=1;
+	    }
+	    //Fifth Question//
+	    System.out.println("What is the “powerhouse” of the cell? Is it:");
+	    //Choices//
+	    System.out.println("(1)Deoxyribonucleic Acid n\ (2)Mitochondria n\ (3)Cristae n\ (4)Ribosome");
+	    int SecondAnswer = Keyboard.readInt();
+	    if(SecondAnswer == 2) {
+		Score+=1;
+	    }
+
+
+
+	    
+	    }
+	}
+	if(Score<3;){
 	    System.out.println("Jeez you def lied (failed quiz)");
 	
 	    return 2;
-      }
 	}
-	*/
-	      }
-	else {
-	    System.out.println("BLalbalbal I've hearfd this one already (In response to long excuse)!");
-	    return 1;
-	    //add 5 energy used to the player
-	}
-	}
-      
-      return 0;
     }
+    */
+}
+else {
+    System.out.println("Fine.");
+    return 1;
+    //add 5 energy used to the player
+}
+}
+      
+return 0;
+}
       
       
       
@@ -49,9 +87,9 @@ public class BioTrivia extends Activity{
       
       
     
-    public void PlayActivity(Player x) {
+public void PlayActivity(Player x) {
 	
-    }
+}
 
 
 }
