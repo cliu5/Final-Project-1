ace0eebe722cd12f00af29adb1136a09156da309
LuckyLuckyLu <alu8@stuy.edu>
2018-01-12 16:08:39 -0500

    Added activities to Activity.java.

--- Player.java
+++ Player.java
@@ -28,41 +28,89 @@ public class Player{
 	    Floor5.add("");
 	    Floor5.add("");
 	    Floor5.add("");
-	    ArrayList<String> Floor1 = new ArrayList<String>();
+	    ArrayList<String> Floor6 = new ArrayList<String>();
 	    Floor6.add("");
 	    Floor6.add("");
 	    Floor6.add("");
-	    ArrayList<String> Floor1 = new ArrayList<String>();
+	    ArrayList<String> Floor7 = new ArrayList<String>();
 	    Floor7.add("");
 	    Floor7.add("");
 	    Floor7.add("");
-	    ArrayList<String> Floor1 = new ArrayList<String>();
+	    ArrayList<String> Floor8 = new ArrayList<String>();
 	    Floor8.add("");
 	    Floor8.add("");
 	    Floor8.add("");
-	    ArrayList<String> Floor1 = new ArrayList<String>();
-	    Floor8.add("");
-	    Floor8.add("");
-	    Floor8.add("");
+	    ArrayList<String> Floor9 = new ArrayList<String>();
+	    Floor9.add("");
+	    Floor9.add("");
+	    Floor9.add("");
+
+
+	    ArrayList<ArrayList<Activity>> MasterActivity = new	ArrayList<ArrayList<Activity>>();
+	    ArrayList<Activity>_Floor1=new ArrayList<Activity>();
+	    Activity STCTrivia = new STCTrivia();
+	    _Floor1.add(BioTrivia);
+	    Activity something = new thing();
+	    _Floor1.add(b);
+	    MasterActivity.add(_Floor1);
+	    ArrayList<Activity>_Floor2=new ArrayList<Activity>();
+	    Activity something = new thing();
+	    _Floor1.add(b);
+	    Activity something = new thing();
+	    _Floor1.add(b);
+	    Activity something = new thing();
+	    _Floor1.add(b);
+	    MasterActivity.add(_Floor2);
+	    ArrayList<Activity>_Floor3=new ArrayList<Activity>();
+	    Activity HisTrivia = new HisTrivia();
+	    _Floor1.add(b);
+	    Activity something = new thing();
+	    _Floor1.add(b);
+	    Activity something = new thing();
+	    _Floor1.add(b);
+	    MasterActivity.add(_Floor3);
+	    ArrayList<Activity>_Floor4=new ArrayList<Activity>();
+	    Activity something = new thing();
+	    _Floor1.add(b);
+	    Activity something = new thing();
+	    _Floor1.add(b);
+	    Activity something = new thing();
+	    _Floor1.add(b);
+	    MasterActivity.add(_Floor4);
+	    ArrayList<Activity>_Floor5=new ArrayList<Activity>();
+	    Activity something = new thing();
+	    _Floor1.add(b);
+	    MasterActivity.add(_Floor5);
+	    ArrayList<Activity>_Floor6=new ArrayList<Activity>();
+	    Activity something = new thing();
+	    _Floor1.add(b);
+	    Activity something = new thing();
+	    _Floor1.add(b);
+	    MasterActivity.add(_Floor6);
+	    ArrayList<Activity>_Floor7=new ArrayList<Activity>();
+	    Activity BioTrivia = new BioTrivia();
+	    _Floor1.add(BioTrivia);
+	    Activity something = new thing();
+	    _Floor1.add(b);
+	    MasterActivity.add(_Floor7);
+	    ArrayList<Activity>_Floor8=new ArrayList<Activity>();
+	    Activity something = new thing();
+	    _Floor1.add(b);
+	    Activity something = new thing();
+	    _Floor1.add(b);
+	    MasterActivity.add(_Floor8);
+	    ArrayList<Activity>_Floor9=new ArrayList<Activity>();	    
+	    Activity twenty48 = new 2048(); //The latter is the classname//
+	    _Floor9.add(twenty48);
+	    MasterActivity.add(_Floor9);
+	    
 
 
 
 
 
 
-
-
-
-	    ArrayList<ArrayList<Activity>> MasterActivity = new 
-		ArrayList<Activity> 
-
-
-
-
-
-
-
-		}
+	}
 
 
 	public void Play(){
