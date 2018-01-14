82765c94dea63b20f3f3f39413c33a2caecdaebb
LuckyLuckyLu <alu8@stuy.edu>
2018-01-13 20:50:58 -0500

    Fixed some more stuff in Player.java.

--- Player.java
+++ Player.java
@@ -11,9 +11,7 @@ public class Player{
 	    ArrayList<String> Floor1 = new ArrayList<String>();
 	    Floor1.add("");
 	    Floor1.add("");
-	    Floor1.add("");
 	    ArrayList<String> Floor2 = new ArrayList<String>();
-	    Floor2.add("");
 	    Floor2.add("");
 	    Floor2.add("");
 	    ArrayList<String> Floor3 = new ArrayList<String>();
@@ -23,25 +21,19 @@ public class Player{
 	    ArrayList<String> Floor4 = new ArrayList<String>();
 	    Floor4.add("");
 	    Floor4.add("");
-	    Floor4.add("");
 	    ArrayList<String> Floor5 = new ArrayList<String>();
-	    Floor5.add("");
 	    Floor5.add("");
 	    Floor5.add("");
 	    ArrayList<String> Floor6 = new ArrayList<String>();
 	    Floor6.add("");
 	    Floor6.add("");
-	    Floor6.add("");
 	    ArrayList<String> Floor7 = new ArrayList<String>();
-	    Floor7.add("");
 	    Floor7.add("");
 	    Floor7.add("");
 	    ArrayList<String> Floor8 = new ArrayList<String>();
 	    Floor8.add("");
 	    Floor8.add("");
-	    Floor8.add("");
 	    ArrayList<String> Floor9 = new ArrayList<String>();
-	    Floor9.add("");
 	    Floor9.add("");
 	    Floor9.add("");
 
