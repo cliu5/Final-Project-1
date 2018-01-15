c3211335a5110caa0a23fed60972f6fdcd3aada5
cliu5 <cliu5@stuy.edu>
2018-01-15 13:58:13 -0500

    Update MathExhibit.java

--- MathExhibit.java
+++ MathExhibit.java
@@ -1,12 +1,13 @@
+import cs1.Keyboard
 public class MathExhibit extends Activity{
   public void PlayActivity(Player x) {
     boolean result = MathExhibit();
     if (result){
       System.out.println("Yay u won...");
-      enery+=2;
+      x.energy+=2;
     }else{
       System.out.println("Awww man  u lost");
-      energy-=1;
+      x.energy-=1;
     }
   }
 
