b9445c7876285ce9b889dbcffaf0790d957f5c56
cliu5 <cliu5@stuy.edu>
2018-01-15 15:47:58 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.ArrayList;
 public class Driver{
 public static void main(String[] args){
-    System.out.println("Welcome to our game");
+    System.out.println("What is your name?");
     String name="Claire";
     Player _player=new Player(name);
     _player.Play();
