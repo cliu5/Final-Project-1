bfcb87bd59f429ceb3c65858117e6d6c6d6d62ba
cliu5 <cliu5@stuy.edu>
2018-01-15 13:59:49 -0500

    Update BioTrivia.java

--- BioTrivia.java
+++ BioTrivia.java
@@ -84,10 +84,10 @@ public class BioTrivia extends Activity{
     boolean result = BioTrivia();
     if (result){
       System.out.println("Yay u won...");
-      enery+=2;
+      x.energy+=2;
     }else{
       System.out.println("Awww man  u lost");
-      energy-=1;
+      x.energy-=1;
     }
   }
 }
