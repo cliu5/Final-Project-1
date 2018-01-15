ecc8f9f8e0d1e968b9253432165b104a6ddbdf86
cliu5 <cliu5@stuy.edu>
2018-01-14 23:10:23 -0500

    Update SaD.java

--- SaD.java
+++ SaD.java
@@ -2,11 +2,11 @@ public class SaD extends Activity{
   public void PlayActivity(Player x) {
     boolean result = SaDStarter();
     if (result){
-      System.out.println(“Yay u won…”);
+      System.out.println("Yay u won…");
       energy+=2;
     }
     else{
-      System.print”Awww man u lost”;
+      System.out.println("Awww man u lost");
       energy-=1;
     }
   }
