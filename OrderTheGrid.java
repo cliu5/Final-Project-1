5df68d3e7649b77cecf6ad9b968ad3eb20f220df
LuckyLuckyLu <alu8@stuy.edu>
2018-01-20 20:01:16 -0500

    Created private variables and constructor.

--- OrderTheGrid.java
+++ OrderTheGrid.java
@@ -1,3 +1,14 @@
 public class OrderTheGrid{
-
+private int[][] Grid;
+  public OrderTheGrid(){
+    Grid = new int[3][3];
+    int[] numbers = {1,2,3,4,5,6,7,8,9};
+    int index = 0;
+    for (int i = 0; i < 3; i++){
+      for (int j = 0; j < 3; j++){
+        index += 1;
+        Grid[i][j] = index;
+      }
+    }
+  }
 }
