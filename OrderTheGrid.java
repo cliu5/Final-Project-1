c852f7c15571c4c8b0955f8f990acb8878e47b84
LuckyLuckyLu <alu8@stuy.edu>
2018-01-21 00:44:22 -0500

    Added two more methods for movement and shifting of tiles. Made driver interactive.

--- OrderTheGrid.java
+++ OrderTheGrid.java
@@ -22,14 +22,12 @@ private int[][] Grid;
     return grid;
   }
   public boolean Finish(){
-    int number = 0;
     boolean Truth = true;
     for (int i = 0; i < 3; i++){
       for (int j = 0; j < 3; j++){
-        if (Grid[i][j] != number){
+        if (Grid[i][j] != (3*i + j + 1)){
           Truth = false;
         }
-        number += 1;
       }
     }
     return Truth;
@@ -68,6 +66,46 @@ private int[][] Grid;
     if (row != 0){
       int value = Grid[row-1][column];
       Grid[row-1][column] = Grid[row][column];
+      Grid[row][column] = value;
+    }
+    else {
+      System.out.println("NOT VALID. Try again.");
+    }
+  }
+   public void ShiftLeft(int x){
+    int row = 0;
+    int column = 0;
+    for (int i = 0; i < 3; i++){
+      for (int j = 0; j < 3; j++){
+        if (Grid[i][j] == x){
+          row = i;
+          column = j;
+        }
+      }
+    }
+    if (column != 0){
+      int value = Grid[row][column-1];
+      Grid[row][column-1] = Grid[row][column];
+      Grid[row][column] = value;
+    }
+    else {
+      System.out.println("NOT VALID. Try again.");
+    }
+  }
+   public void ShiftRight(int x){
+    int row = 0;
+    int column = 0;
+    for (int i = 0; i < 3; i++){
+      for (int j = 0; j < 3; j++){
+        if (Grid[i][j] == x){
+          row = i;
+          column = j;
+        }
+      }
+    }
+    if (column != 2){
+      int value = Grid[row][column+1];
+      Grid[row][column+1] = Grid[row][column];
       Grid[row][column] = value;
     }
     else {
--- OrderTheGridDriver.java
+++ OrderTheGridDriver.java
@@ -5,7 +5,31 @@ public class OrderTheGridDriver{
     OrderTheGrid test = new OrderTheGrid();
     System.out.println(test);
     System.out.println(test);
-    test.ShiftDown(4);
+    test.ShiftLeft(3);
+    while (!test.Finish()){
+      System.out.println(test);
+      int Piece = Keyboard.readInt();
+      String Shift = Keyboard.readString();
+
+      if ((Piece > 0 && Piece < 10) && (Shift.equals("d") || Shift.equals("u") || Shift.equals("l") || Shift.equals("r"))){
+        if (Shift.equals("d")){
+          test.ShiftDown(Piece);
+            }
+        if (Shift.equals("u")){
+          test.ShiftUp(Piece);
+        }
+        if (Shift.equals("l")){
+          test.ShiftLeft(Piece);
+        }
+        if (Shift.equals("r")){
+          test.ShiftRight(Piece);
+        }
+      }else {
+        System.out.println("Not valid direction. Up = u, Down = d, Left = l, Right = r.");
+      }
+      System.out.println(test.Finish());
+    }
+    //test.ShiftDown(4);
     System.out.println(test);
     test.ShiftDown(4);
     System.out.println(test);
