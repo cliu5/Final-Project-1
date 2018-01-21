2ae9211d919e76a14e643a720fbeb272cb53a183
LuckyLuckyLu <alu8@stuy.edu>
2018-01-20 20:09:39 -0500

    Added ShiftDown method.

--- OrderTheGrid.java
+++ OrderTheGrid.java
@@ -21,4 +21,16 @@ private int[][] Grid;
     }
     return grid;
   }
+  public void ShiftDown(int x){
+    int row = 0;
+    int column = 0;
+    for (int i = 0; i < 3; i++){
+      for (int j = 0; j < 3; j++){
+        if (Grid[i][j] == x){
+          row = i;
+          column = j;
+        }
+      }
+    }
+  }
 }
