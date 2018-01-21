fa3e7b268a5fd5b44a1bddfb24a4b7bf01510101
LuckyLuckyLu <alu8@stuy.edu>
2018-01-20 20:13:16 -0500

    Added test cases for written methods.

--- OrderTheGridDriver.java
+++ OrderTheGridDriver.java
@@ -1,4 +1,14 @@
 import cs1.Keyboard;
 
 public class OrderTheGridDriver{
+  OrderTheGrid test = new OrderTheGrid();
+  System.out.println(test);
+  test.ShiftDown(4);
+  System.out.println(test);
+  test.ShiftDown(4);
+  System.out.println(test);
+  test.ShiftUp(5);
+  System.out.println(test);
+  test.ShiftUp(5);
+  System.out.println(test);
 }
